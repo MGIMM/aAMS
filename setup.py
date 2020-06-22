6fba2c5f764d3a7e875c3f216616b8ff7032a972
mgimm <duqiming2004@gmail.com>
2020-06-22 11:20:09 +0200

    add np in setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 #from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
-#import numpy as np
+import numpy
 
 
 sourcefiles = ['./aAMS/aAMS.pyx']
 
-extensions = [Extension("aAMS", sourcefiles)]
+extensions = [Extension("aAMS",sourcefiles,include_dirs=[numpy.get_include()])]
 
 
 setup(
-    ext_modules=cythonize(extensions),
+    ext_modules=cythonize(extensions, compiler_directives={'language_level' : "3"}),
     name='aAMS',
     version='0.0.1',
     description='Adaptive Multilevel Splitting under asymmetric SMC framework with variance estimation.',
